f935e9bf825a7d76e3ba0ff4181174bdd387614e
Home <home@Siddharthas-Mac-mini.local>
2019-10-19 22:58:53 +0530

    little refactoring

--- src/test/java/com/rabobank/statementprocessor/ApplicationTests.java
+++ src/test/java/com/rabobank/statementprocessor/ApplicationTests.java
@@ -1,10 +1,10 @@
-package com.rabobank.statementprocessor.customerstatementprocessorbatch;
+package com.rabobank.statementprocessor;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CustomerStatementProcessorBatchApplicationTests {
+class ApplicationTests {
 
 	@Test
 	void contextLoads() {
